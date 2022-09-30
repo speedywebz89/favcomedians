27619b089d1d6f6d40d40bf913d48938fdca15a5
Tyler Gonzalez <106620557+speedywebz89@users.noreply.github.com>
2022-09-30 06:06:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-//USING FORLOOPS** to manage our drum buttons
-
-
-//adding a forLoop to handle the clicks for every piece of the drum set.
-// one way of doing this is by using this line of code below.
-//because we don't want the forLoop to continuously cycle through every button on the page we with be more specific by only targeting the buttons with 
-//the class of ".drum". I am calling ".length" just to figure out how many buttons i have with the class of drum
-
-//for (var i = 0; i<document.querySelectorAll(".drum").length)
 
 
 
 
-//I want to seperate the two lines of code by first creating a variable to determine the amount of times i want my code to loop
-//document.querySelectorAll every button with the class of ".drum"
-//  .length to figure out how many buttons are in the class of ".drum"
-// If this was a value returned to the console.log it would return an array of numbers. [0,1,2,3,4,5,6,]
+
+
+
 
 
  document.body.style.backgroundColor = "#82adac";
@@ -23,8 +13,8 @@
 var numOfQuotes = document.querySelectorAll(".quotes").length;
 
 
-// now i will create a forLoop and add the return variable numOfDrumButtons value.
-// then we will increment the value of "i" by 1 each time until the specified numOfDrumButtons is met.
+// now i will create a forLoop and add the return variable numOfQuotes value.
+// then we will increment the value of "i" by 1 each time until the specified numOfQuotes is met.
 
 for (var i = 0; i < numOfQuotes; i++) {
 
